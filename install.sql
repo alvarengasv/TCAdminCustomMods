9ee91d190fb6690ba75f86c8fce4d963e580edfa
Alex Redding <Alexredding180@gmail.com>
2021-02-01 10:44:33 +0000

    Install.sql update

--- install.sql
+++ install.sql
@@ -11,7 +11,7 @@ VALUES (2, 'b48cfbc9-7acc-4980-89c4-2b6a1f784aa0', 'Curse Configuration',
   <add key="AR_COMMON:ConfigurationView" value="CurseProvider" type="System.String,mscorlib" />
 </values>');
 
--- ---------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
 
 create table tcmodule_custom_mod_providers
 (
@@ -25,7 +25,7 @@ create table tcmodule_custom_mod_providers
     app_data              text        null
 );
 
--- ---------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
 
 INSERT INTO tcmodule_custom_mod_providers (id, name, typeName, configurationModuleId, configurationId, view,
                                            app_data)
@@ -36,7 +36,7 @@ INSERT INTO tcmodule_custom_mod_providers (id, name, typeName, configurationModu
 VALUES (2, 'Curse', 'TCAdminCustomMods.Providers.CurseProvider, TCAdminCustomMods',
         'b48cfbc9-7acc-4980-89c4-2b6a1f784aa0', 2, 'CurseProvider', '');
 
--- ---------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
 
 INSERT INTO tc_page_icons (icon_id, module_id, page_id, linked_page_id, linked_page_module_id, display_name,
                            description, icon, url, display_sql, user_type, selected_user_type, view_order,
@@ -52,7 +52,7 @@ VALUES (4891, 'd3b2aa93-7e2b-4e0d-8080-67d14b2fa8a9', 3, 2, 'b48cfbc9-7acc-4980-
         'Manage the custom mod providers this game supports', 'MenuIcons/GameHosting/Mods.png',
         '/Game/CustomModsAdmin/Index/$[Game.GameId]', null, 0, null, 975, 1, null, null, null, null, null);
 
--- ---------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
 
 INSERT INTO tc_site_map (page_id, module_id, parent_page_id, parent_page_module_id, category_id, url, mvc_url,
                          controller, action, display_name, page_small_icon, panelbar_icon, show_in_sidebar,
@@ -73,7 +73,7 @@ VALUES (2, 'b48cfbc9-7acc-4980-89c4-2b6a1f784aa0', 3, 'd3b2aa93-7e2b-4e0d-8080-6
         'MenuIcons/GameHosting/Mods16x16.png', 0, 1000,
         '({07405876-e8c2-4b24-a774-4ef57f596384,0})({d3b2aa93-7e2b-4e0d-8080-67d14b2fa8a9,1})', '', null, null, '');
 
--- ---------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
 
 INSERT INTO tc_modules (module_id, display_name, version, enabled, config_page, component_directory,
                         security_class)
